3187c047a844e5b3f626c7f424b46016ba20fe05
KartikeyPushkarna <kartikeypushkarna1211@gmail.com>
2022-12-30 06:49:10 -0800

    Website Not Completed yet

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Home from "./Home";
+import {
+  BrowserRouter as Router,
+  Route, Routes
+} from "react-router-dom";
+import About from "./Components/AboutUs"
+import Contact from "./Components/ContactUs"
+import Events from "./Components/EventsBro";
+import Stories from "./Components/StoriesBro"
+// import Error from "./Components/Error";
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route exact path="/"
+          element={<Home />} />
+          <Route path="/About" element={<About/>}/>
+          <Route path="/Events" element={<Events/>}/>
+          <Route path="/Contact"  element={<Contact/>}/>
+          <Route path="/Stories"  element={<Stories/>}/>
+          {/* <Route element={<Error/>}/> */}
+        </Routes>
+      </Router>
+    </>
   );
-}
+};
 
 export default App;
